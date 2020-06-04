6deeff6a6afa9080d798271f150f46e808203306
sheltoG <36433204+sheltoG@users.noreply.github.com>
2020-06-03 20:43:05 -0700

    Added zero seconds

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-var timer = 66;
+var timer = 10;
 var interval = setInterval(myTimer,1000);
-  
+ 
+function convertSeconds(timer)
+{
+var min = Math.floor (timer / 60);
+var sec = timer % 60;
+if (sec < 10) {
+		sec = "0" + sec;
+	}
+return min + ':' + sec;
+}
+
+
 function myTimer()                         
 {
+document.getElementById("timer").innerHTML = convertSeconds(timer);
 timer--; 
-document.getElementById("timer").innerHTML = timer;
 if (timer === 0)                        
  clearInterval(interval);
 }
