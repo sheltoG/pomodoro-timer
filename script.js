a703c752e99a26d8bba5a3c7cc0c0465b4405bf3
sheltoG <36433204+sheltoG@users.noreply.github.com>
2020-06-04 21:13:15 -0700

    CSS Styling

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-var timer = 10;
+var timer = 1500;
+var secondsRemaining;
 var interval = setInterval(myTimer,1000);
  
 function convertSeconds(timer)
 {
 var min = Math.floor (timer / 60);
-var sec = timer % 60;
+var sec = timer - (min * 60);
+  
+//add a leading zero (as a string value) if seconds less than 10
 if (sec < 10) {
 		sec = "0" + sec;
 	}
 return min + ':' + sec;
 }
 
-
+//
 function myTimer()                         
 {
 document.getElementById("timer").innerHTML = convertSeconds(timer);
 timer--; 
-if (timer === 0)                        
- clearInterval(interval);
+if (timer < 0)                        
+  clearInterval(interval);
 }
